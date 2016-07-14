b8cfc5dbbe6653161955ed6eec939a5d7462a920
TheRealWeebKid <FREEBEEZY@Barrys-MacBook-Air.local>
2016-07-14 14:27:40 -0700

    snapchat 4 weebs

---
+++ Weebchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weebchat
+//
+//  Created by Barry Esteen on 7/14/16.
+//  Copyright © 2016 Barry Esteen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
